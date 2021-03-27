08bdcaf8f1f5b2fb0e84e2b442738ce7b1fc8545
cesarramos95 <cesarramos.aug@gmail.com>
2021-03-27 16:10:00 -0300

    Communicating application events

---
+++ client/src/constants.js
@@ -0,0 +1,15 @@
+export const constants = {
+  events: {
+    app: {
+      MESSAGE_SENT: 'message_sent',
+      MESSAGE_RECEIVED: 'message:received',
+      ACTIVITYLOG_UPDATED: 'activityLog:update',
+      STATUS_UPDATED: 'status_updated'
+    },
+
+    socket: {
+      JOIN_ROOM: 'joinRoom',
+      MESSAGE: 'message'
+    }
+  }
+}
---
+++ client/src/terminalController.js
@@ -0,0 +1,103 @@
+import ComponentsBuilder from "./components.js"
+import { constants } from "./constants.js";
+
+export default class TerminalController {
+  #usersCollors = new Map()
+
+  constructor() {}
+
+  #pickCollor() {
+    return `#${((1 << 24) * Math.random() | 0).toString(16)}-fg`;
+  }
+
+  #getUserCollor(userName) {
+    if (this.#usersCollors.has(userName))
+      return this.#usersCollors.get(userName);
+    
+    const collor = this.#pickCollor();
+    this.#usersCollors.set(userName, collor);
+
+    return collor;
+  }
+
+  #onInputReceived(eventEmitter) {
+    return function() {
+      const message = this.getValue();
+      eventEmitter.emit(constants.events.app.MESSAGE_SENT, message);
+      this.clearValue();
+    }
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return msg => {
+      const { userName, message } = msg;
+      const collor = this.#getUserCollor(userName);
+
+      chat.addItem(`{${collor}}{bold}${userName}{/}: ${message}`);
+
+      screen.render()
+    }
+  }
+
+  #onLogChanged({ screen, activityLog }) {
+    
+    return msg => {
+      const [userName] = msg.split(/\s/);
+      const collor = this.#getUserCollor(userName);
+      activityLog.addItem(`{${collor}}{bold}${msg.toString()}{/}`);
+
+      screen.render();
+    }
+  }
+
+  #onStatusChanged({ screen, status }) {
+
+    return users => {
+
+
+      // Vamos pegar o primeiro elemento da lista
+      const { content } = status.items.shift();
+      status.clearItems();
+      status.addItem(content);
+
+      users.forEach(userName => {
+        const collor = this.#getUserCollor(userName);
+        status.addItem(`{${collor}}{bold}${userName}{/}`)
+      })
+
+      screen.render();
+    }
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on(constants.events.app.MESSAGE_RECEIVED, this.#onMessageReceived(components));
+    eventEmitter.on(constants.events.app.ACTIVITYLOG_UPDATED, this.#onLogChanged(components));
+    eventEmitter.on(constants.events.app.STATUS_UPDATED, this.#onStatusChanged(components));
+  }
+
+  async initializeTable(eventEmitter) {
+    const components = new ComponentsBuilder()
+      .setScreen({ title: 'HackerChat - César Augusto' })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .setActivityLogComponent()
+      .setStatusComponent()
+      .build()
+
+    this.#registerEvents(eventEmitter, components);
+
+    components.input.focus();
+    components.screen.render();
+
+    // setInterval(() => {
+      // const users = ['cesarRamos95']
+      // eventEmitter.emit(constants.events.app.STATUS_UPDATED, users)
+      // users.push('Thais')
+      // eventEmitter.emit(constants.events.app.STATUS_UPDATED, users)
+      // users.push('Jacare', 'alface')
+      // eventEmitter.emit(constants.events.app.STATUS_UPDATED, users)
+      // users.push('Dipsy', 'zecolmeia001')
+    // }, 1000);
+  }
+}
